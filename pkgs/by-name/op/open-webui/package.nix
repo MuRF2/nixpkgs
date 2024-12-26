73d40ee5254b3f12c45506f902f5d0d0978f8f96
MuRF2 <michi.lueders@t-online.de>
2024-12-26 20:03:10 +0100

    open-webui: 0.4.6 -> 0.5.1

--- pkgs/by-name/op/open-webui/package.nix
+++ pkgs/by-name/op/open-webui/package.nix
@@ -7,21 +7,19 @@
 }:
 let
   pname = "open-webui";
-  version = "0.4.6";
+  version = "0.5.1";
 
   src = fetchFromGitHub {
     owner = "open-webui";
     repo = "open-webui";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Zzytv2OLy3RENNWzRjjDh7xnJyX+H9/dh1Xj2HIsn6I=";
+    hash = "sha256-BWQETh+1yL5lV96fq5dN/8COaVP2usPDsKlN+cHAZlk=";
   };
 
   frontend = buildNpmPackage {
     inherit pname version src;
 
-    NODE_OPTIONS = "--max-old-space-size=4096";
-
-    npmDepsHash = "sha256-36GdyqKcqhOYi1kRwXe0YTOtwbVUcEvLPPYy/A0IgE0=";
+    npmDepsHash = "sha256-svxOnKXtWjltjOnKCXj53z5aSrVxzH70WijfiYJ3Ka8=";
 
     # Disabling `pyodide:fetch` as it downloads packages during `buildPhase`
     # Until this is solved, running python packages from the browser will not work.
@@ -32,6 +30,7 @@ let
 
     env.CYPRESS_INSTALL_BINARY = "0"; # disallow cypress from downloading binaries in sandbox
     env.ONNXRUNTIME_NODE_INSTALL_CUDA = "skip";
+    env.NODE_OPTIONS = "--max-old-space-size=8192";
 
     installPhase = ''
       runHook preInstall
@@ -82,6 +81,7 @@ python312.pkgs.buildPythonApplication rec {
     docx2txt
     duckduckgo-search
     einops
+    emoji # This dependency is missing in upstream's pyproject.toml
     extract-msg
     fake-useragent
     fastapi
@@ -92,9 +92,11 @@ python312.pkgs.buildPythonApplication rec {
     ftfy
     google-generativeai
     googleapis-common-protos
+    iso-639
     langchain
     langchain-chroma
     langchain-community
+    langdetect
     langfuse
     ldap3
     markdown
@@ -146,8 +148,11 @@ python312.pkgs.buildPythonApplication rec {
 
   makeWrapperArgs = [ "--set FRONTEND_BUILD_DIR ${frontend}/share/open-webui" ];
 
-  passthru.tests = {
-    inherit (nixosTests) open-webui;
+  passthru = {
+    tests = {
+      inherit (nixosTests) open-webui;
+    };
+    updateScript = ./update.sh;
   };
 
   meta = {
